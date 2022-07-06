db1d4fd842122aee21a0941006dcfe799cb4ee98
Juan Feris <feris.juan@gmail.com>
2022-07-06 18:07:04 -0500

    mfe demo

---
+++ home/src/Footer.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default function Header() {
+  return <div className="mt-5 p-5 bg-blue-500 text-white text-3xl font-bold">
+    Only The Best Spinner 
+  </div>
+}
